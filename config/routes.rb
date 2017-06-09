139024a59129ed6eea349d01be738aaba5ebf37b
--replace-all <prem.saha@untroddenlabs.com>
2017-06-09 11:31:03 +0530

    Set the routes contraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json }, consttaints: { subdomain: 'api'}, path: '/' do
+  end
 end
