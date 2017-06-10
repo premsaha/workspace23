387f4b79b8d4e9c276707f00f50196e4d0fde080
--replace-all <prem.saha@untroddenlabs.com>
2017-06-10 12:47:59 +0530

    Adds destroy action to the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/'  do
-    scope module: :v1,
-              constraints: ApiConstraints.new(version: 1, default: true) do
-      # We are going to list our resources here
-      resources :users, :only => [:show]
+      scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+  # We are going to list our resources here
+      resources :users, :only => [:show, :create, :update, :destroy]
     end
   end
 end
